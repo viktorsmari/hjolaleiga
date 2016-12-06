87e9c6a42cdaba492709904bb3492df0f54c51c8
Viktor Smari <viktorsmari@gmail.com>
2016-12-06 20:24:28 +0100

    Permit admin to edit Rentals

--- app/admin/rental.rb
+++ app/admin/rental.rb
@@ -3,15 +3,15 @@ ActiveAdmin.register Rental do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+#permit_params :start_date, :end_date, :email, :bicycle
 #
 # or
 #
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params do
+  permitted = [:start_date, :end_date, :email, :bicycle]
+  #permitted << :other if params[:action] == 'create' && current_user.admin?
+  permitted
+end
 
 
 end
